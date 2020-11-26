a0090b0235ec626811879dba260f40f5203882c2
Thomas Ricatte <thomas@ricatte.fr>
2020-11-26 11:59:31 +0100

    Adding GPU Queues option

--- r3d3/experiment_launcher.py
+++ r3d3/experiment_launcher.py
@@ -1,9 +1,9 @@
 import argparse
 import os
-import re
 import subprocess
 import sys
 import time
+from typing import List, Optional
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 
@@ -13,7 +13,12 @@ root_dir = "{}/..".format(os.path.dirname(os.path.abspath(__file__)))
 
 
 class ExperimentLauncher(object):
-    def __init__(self, db_path, forward_nb_processes=True):
+    def __init__(
+        self,
+        db_path: str,
+        forward_nb_processes: bool = True,
+        gpu_queues: Optional[List[int]] = None,
+    ):
         if len(db_path) > 0:
             self.db = ExperimentDB(db_path)
         else:
@@ -21,6 +26,7 @@ class ExperimentLauncher(object):
         self.experiment_id = None
 
         self.forward_nb_processes = forward_nb_processes
+        self.gpu_queues = gpu_queues
 
     def run(
         self, experiment_plan: R3D3ExperimentPlan, max_nb_processes: int, debug: bool
@@ -48,16 +54,18 @@ class ExperimentLauncher(object):
 
             return launch_command_line
 
-        # Creating env for the runs
-        env = os.environ.copy()
-        if experiment_plan.debug:
-            print("Using env {}".format(env))
-
         nb_tests = len(experiment_plan.experiments)
         print("%d experiments to launch..." % nb_tests)
 
         # Creating executors with max nb processes from the config
-        executor = ThreadPoolExecutor(max_workers=max_nb_processes)
+        if self.gpu_queues is None:
+            executor = ThreadPoolExecutor(max_workers=max_nb_processes)
+        else:
+            executors = {
+                gpu_id: ThreadPoolExecutor(max_workers=max_nb_processes)
+                for gpu_id in self.gpu_queues
+            }
+            gpu_ids = sorted(list(executors.keys()))
 
         # Running the tests
         now = datetime.now()
@@ -66,6 +74,9 @@ class ExperimentLauncher(object):
         futures = list()
 
         for run_id, experiment in enumerate(experiment_plan.experiments):
+            # Env for the run
+            env = os.environ.copy()
+
             # The python binary is available in sys.executable
             args = ["{} {}".format(sys.executable, f"{experiment.binary}")]
             for key in experiment.config:
@@ -91,7 +102,15 @@ class ExperimentLauncher(object):
                 )
 
             command = " ".join(args)
-            future = executor.submit(
+
+            if self.gpu_queues is None:
+                my_executor = executor
+            else:
+                my_gpu = gpu_ids[run_id % len(gpu_ids)]
+                my_executor = executors[my_gpu]
+                env["CUDA_VISIBLE_DEVICES"] = str(my_gpu)
+
+            future = my_executor.submit(
                 launcher_with_environment(env, debug=debug), command
             )
             futures.append(future)
@@ -103,7 +122,10 @@ class ExperimentLauncher(object):
 
 
 def main(
-    experiment_file: str, forward_nb_processes: bool, debug: bool
+    experiment_file: str,
+    forward_nb_processes: bool,
+    debug: bool,
+    gpu_queues: Optional[List[int]] = None,
 ) -> ExperimentLauncher:
     print(experiment_file)
 
@@ -114,7 +136,9 @@ def main(
     my_experiment_plan: R3D3ExperimentPlan = variables["experiment_plan"]
 
     my_launcher = ExperimentLauncher(
-        db_path=my_experiment_plan.db_path, forward_nb_processes=forward_nb_processes
+        db_path=my_experiment_plan.db_path,
+        forward_nb_processes=forward_nb_processes,
+        gpu_queues=gpu_queues,
     )
     my_launcher.run(
         experiment_plan=my_experiment_plan,
@@ -132,10 +156,17 @@ def main_cli():
         "--no_nb_proc", action="store_false", dest="forward_nb_processes"
     )
     parser.add_argument("--debug", action="store_true", dest="debug")
+    parser.add_argument("--gpu_queues", type=str, default="")
     args = parser.parse_args()
+
+    if len(args.gpu_queues) == 0:
+        args.gpu_queues = None
+    else:
+        args.gpu_queues = args.gpu_queues.split("_")
 
     main(
         experiment_file=args.experiment_file,
         forward_nb_processes=args.forward_nb_processes,
         debug=args.debug,
+        gpu_queues=args.gpu_queues,
     )
